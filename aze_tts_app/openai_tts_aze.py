e4c741528d4b9377a941dffbbdb14a4ac2fc12fc
Khan Safarov <safarovrafiq9@gmail.com>
2024-02-20 10:37:32 +0400

    edited speech convertion

--- aze_tts_app/openai_tts_aze.py
+++ aze_tts_app/openai_tts_aze.py
@@ -1,23 +1,30 @@
- # onyx, shimmer and nova are the best one.
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.conf import settings
+from openai import OpenAI
+from django.http import JsonResponse
+from urllib.parse import urlparse
 from pathlib import Path
-from openai import OpenAI 
 
 @api_view(['POST'])
 def generate_audio(request):
-    text= request.data.get("text", '')
+    text = request.data.get("text", '')
     voice = request.data.get("voice", 'onyx')
-    
-    client = OpenAI(api_key= "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")
+
+    client = OpenAI(api_key="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")
 
     speech_file_path = Path(__file__).parent / "speech.mp3"
     response = client.audio.speech.create(
-      model="tts-1",
-      voice=voice, 
-      input= text
+        model="tts-1",
+        voice=voice,
+        input=text
     )
 
     response.stream_to_file(speech_file_path)
 
-    return Response({'message': "Audio generated successfully"})
+    audio_file_url = f"{settings.MEDIA_URL}speech.mp3"
+
+    return JsonResponse({
+        'message': "Audio generated successfully",
+        'audio_file_url': audio_file_url,
+    })
--- aze_tts_app_project/urls.py
+++ aze_tts_app_project/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from aze_tts_app.openai_tts_aze import generate_audio
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/generate_audio/', generate_audio, name='generate_audio'),
-
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
